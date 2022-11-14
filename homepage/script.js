e54e6702f7aa6bae7a32540aa57b7be248e02388
Cosmin <cosmin.matei@eminescubucuresti.ro>
2022-11-14 23:03:31 +0200

    Update script.js

--- homepage/script.js
+++ homepage/script.js
@@ -51,3 +51,17 @@ var x = setInterval(function() {
 }, 1000);
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    var elements = document.getElementsByTagName("INPUT");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].oninvalid = function(e) {
+            e.target.setCustomValidity("");
+            if (!e.target.validity.valid) {
+                e.target.setCustomValidity("Acest camp nu poate fi lasat liber bosule!");
+            }
+        };
+        elements[i].oninput = function(e) {
+            e.target.setCustomValidity("");
+        };
+    }
+})
